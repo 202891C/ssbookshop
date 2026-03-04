3dab39098014af5f6b2c1c3cd2ec47c6fc921f71
Mark <r.mark.ng@gmail.com>
2026-03-05 00:46:14 +0800

    Edited database to auto_increment id, inserted image into book

--- SQL Scripts/Initialization Script.sql
+++ SQL Scripts/Initialization Script.sql
@@ -1,23 +1,25 @@
+Drop database ssbookshop;
 create database if not exists ssbookshop;
 show databases;
 
 use ssbookshop;
 
 drop table if exists book; 
-create table book (bookID int primary key, title varchar(50), price float, qty int);
+create table book (bookID int not null primary key auto_increment, title varchar(50), price float, qty int, image varchar(255));
+ALTER TABLE book AUTO_INCREMENT=1001;
 
 drop table if exists author, book_author, customer, customer_order; 
-create table author (authorID int primary key, name varchar(50), email varchar(50));
+create table author (authorID int not null primary key auto_increment, name varchar(50), email varchar(50));
+ALTER TABLE author AUTO_INCREMENT=101;
 create table book_author(authorID int NOT NULL, bookID int NOT NULL, 
 	foreign key(authorID) references author(authorID), 
     foreign key(bookID) references book(bookID));
 
-create table customer (custID int primary key, name varchar(50), email varchar(50), phone_no int);
+create table customer (custID int not null primary key auto_increment, name varchar(50), email varchar(50), phone_no int);
 create table customer_order (custID int NOT NULL, bookID int NOT NULL, qty int NOT NULL, 
 	foreign key(custID) references customer(custID), 
     foreign key(bookID) references book(bookID)); 
     
-
 show tables;
 describe book;
 describe book_author;
@@ -25,27 +27,27 @@ describe author;
 describe customer_order;
 describe customer;
 
-insert into book values (1001, 'Java for dummies', 11.11, 11);
-insert into book values (1002, 'More Java for dummies', 22.22, 22);
-insert into book values (1003, 'More Java for more dummies', 33.33, 33);
-insert into book values (1004, 'Expensive Book', 100.99, 99);
-insert into book values (1005, 'Null and Void', 30.00, 0);
+insert into book (title, price, qty, image) values ('Java for dummies', 11.11, 11, "images/book-blue.jpg");
+insert into book (title, price, qty) values ('More Java for dummies', 22.22, 22);
+insert into book (title, price, qty) values ('More Java for more dummies', 33.33, 33);
+insert into book (title, price, qty) values ('Expensive Book', 100.99, 99);
+insert into book (title, price, qty) values ('Null and Void', 30.00, 0);
 
-insert into author values(1, 'Tan Ah Teck', 'teck@nowhere.com');
-insert into author values(2, 'Mohamed Ali', 'ali@somewhere.com');
-insert into author values(3, 'Kumar', 'kumar@abc.com');
-insert into author values(4, 'Kelvin Jones', 'kelvin@xyz.com');
+insert into author (name, email) values('Tan Ah Teck', 'teck@nowhere.com');
+insert into author (name, email) values('Mohamed Ali', 'ali@somewhere.com');
+insert into author (name, email) values('Kumar', 'kumar@abc.com');
+insert into author (name, email) values('Kelvin Jones', 'kelvin@xyz.com');
 
-insert into book_author values (1, 1001);
-insert into book_author values (2, 1001);
-insert into book_author values (3, 1001);
-insert into book_author values (1, 1002);
-insert into book_author values (3, 1002);
-insert into book_author values (2, 1003);
-insert into book_author values (2, 1004);
-insert into book_author values (3, 1005);
+insert into book_author values (101, 1001);
+insert into book_author values (102, 1001);
+insert into book_author values (103, 1001);
+insert into book_author values (101, 1002);
+insert into book_author values (103, 1002);
+insert into book_author values (102, 1003);
+insert into book_author values (102, 1004);
+insert into book_author values (103, 1005);
 
-insert into customer values (1, 'Mr Person', 'popipa@pipopa.com', 2802026);
+insert into customer (name, email, phone_no) values ('Mr Person', 'popipa@pipopa.com', 2802026);
 insert into customer_order values (1, 1001, 10);
 
 select * from book;
